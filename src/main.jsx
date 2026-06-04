6543b97e0f3848b28776f65f77d8f54d62a36d72
munishreddy1-creator <munish.reddy1@gmail.com>
2026-06-04 13:00:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-Provider } from './context/AuthContext'
+import { Provider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider>
       <App />
+    </Provider>
   </React.StrictMode>,
 )
